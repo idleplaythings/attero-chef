fbec2e5d3a5572f7bd7a85cb03c1969e068fc212
Niko Nevala <niko@nevala.fi>
2013-03-11 22:16:25 +0000

    update dev server play version, make dev postgres accessible from network

--- roles/dev_server.rb
+++ roles/dev_server.rb
@@ -15,17 +15,33 @@ env_run_lists(
   "dev" => all_env
 )
 
+default_attributes(
+  :postgresql => {
+    :pg_hba => [
+      {:type => 'local', :db => 'all', :user => 'postgres', :addr => nil, :method => 'ident'},
+      {:type => 'local', :db => 'all', :user => 'all', :addr => nil, :method => 'ident'},
+      {:type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'md5'},
+      {:type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'md5'},
+      # todo read network address from the node.. or something
+      {:type => 'host', :db => 'all', :user => 'all', :addr => '192.168.123.0/24', :method => 'md5'},
+    ],
+    :config => {
+      :listen_addresses => '192.168.123.123'
+    }
+  }
+)
+
 override_attributes(
   :scala => {
     :base_url => "http://www.scala-lang.org/downloads/distrib/files",
     :base_dir => "/opt/scala",
     :version => "2.10.0",
-    :checksum => "a57bc12100671253d1ebc4a993e4da1896ff39022ce70c6699986d10644ac9a1"
+    :checksum => "97b0ba48a0165a1451b80b1a5d7a906d8fb5429fbefe996feaa72cc1438f6095"
   },
   :play => {
-    :base_url => "http://download.playframework.org/releases",
+    :base_url => "http://downloads.typesafe.com/play",
     :base_dir => "/opt/play",
-    :version => "2.1-RC1",
+    :version => "2.1.0",
     :checksum => "62a963389c8e75bad83eeb07c3699ccca8de4f40e416974cf56f1d0d45e83bce"
   },
   :authorization => {
