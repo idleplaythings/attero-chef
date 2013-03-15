e9943c9e50536b8d2cd773d3c2210597f37598fd
Niko Nevala <niko@nevala.fi>
2013-03-15 23:19:39 +0000

    copy known_hosts and ssh keys into place in jenkins-setup

--- cookbooks/jenkins-setup/recipes/default.rb
+++ cookbooks/jenkins-setup/recipes/default.rb
@@ -6,3 +6,28 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# Set up known hosts for Jenkins SSH
+cookbook_file "#{node[:jenkins][:server][:home]}/.ssh/known_hosts" do
+  source "jenkins_known_hosts"
+  mode 0644
+  owner "jenkins"
+  group "jenkins"
+end
+
+# Set up Jenkins SSH keys
+ci = Chef::EncryptedDataBagItem.load("secrets", "ci")
+
+file "#{node[:jenkins][:server][:home]}/.ssh/id_rsa" do
+  mode 0600
+  owner "jenkins"
+  group "jenkins"
+  content ci["ssh"]["private_key"]
+end
+
+file "#{node[:jenkins][:server][:home]}/.ssh/id_rsa.pub" do
+  mode 0644
+  owner "jenkins"
+  group "jenkins"
+  content ci["ssh"]["public_key"]
+end
