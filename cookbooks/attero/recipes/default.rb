f2bbea721da2a7d3db0433c9a47eff81eceea30b
Niko Nevala <niko@nevala.fi>
2013-03-16 21:50:03 +0000

    create attero cookbook

---
+++ cookbooks/attero/recipes/default.rb
@@ -0,0 +1,43 @@
+#
+# Cookbook Name:: attero
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+base_dir    = "/opt/supybot"
+
+user        = "attero"
+owner       = "attero"
+group       = "attero"
+
+
+# todo only if user doesn't exist
+user "#{user}" do
+  comment "attero user"
+  system true
+  shell "/bin/sh"
+  home "/home/#{user}"
+end
+
+# Base directory
+directory "#{base_dir}" do
+  mode 00775
+  owner "#{owner}"
+  group "#{group}"
+  action :create
+  recursive true
+end
+
+# Upstart configuration
+template "/etc/init/attero.conf" do
+  source "upstart_service.conf.erb"
+  mode 0775
+  owner "#{owner}"
+  group "#{group}"
+  variables(
+    # :config_var => node[:configs][:config_var]
+  )
+end
